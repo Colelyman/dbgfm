6fc9f6941ee580915da4b4c56bd41a6385a01842
colelyman <cole@colelyman.com>
2017-07-03 16:03:25 -0600

    Updated run_bwtdisk.sh to have a flexible path to the bwtdisk install location.

--- run_bwtdisk.sh
+++ run_bwtdisk.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -eu
 
+# Set the path to bwtdisk
+BWTDISK=../bwtdisk
+
 # Prepare the file by concatenating the contigs/sequences into one long string
 # separated by $
 ./bwtdisk-prepare $1 > $1.joined
 
 # Reverse the text, because bwtdisk constructs the BWT of the reverse text by default.
-text_rev -vvv $1.joined $1.joined.rev
+$BWTDISK/text_rev -vvv $1.joined $1.joined.rev
 
 # Generate the BWT.
-bwte -vvv $1.joined.rev
+$BWTDISK/bwte -vvv $1.joined.rev
 
 # Rename the bwt
 mv $1.joined.rev.bwt `basename $1 .fa`.bwtdisk
+
+# Remove the intermediary files
+rm $1.joined $1.joined.rev
